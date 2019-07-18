73dea2e93ee7e899f40749e10eb1935cf11b2965
Aya <vostroknutov.alexander@gmail.com>
2019-07-18 11:00:49 +0400

    Added Redux

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,25 @@ import Landing from './components/layout/Landing'
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
+import { Provider } from 'react-redux'
+import store from './store'
+
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
-          <Switch>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Switch>
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   )
 }
 
